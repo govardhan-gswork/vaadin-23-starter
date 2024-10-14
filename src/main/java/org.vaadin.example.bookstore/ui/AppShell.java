d061bc092e4441e5fb87801344bedff937008a66
govardhan-gswork <govardhan.gswork@gmail.com>
2024-10-14 12:17:59 +0530

    Delete src/main/java/org.vaadin.example.bookstore directory

--- src/main/java/org.vaadin.example.bookstore/ui/AppShell.java
+++
@@ -1,8 +0,0 @@
-package org.vaadin.example.bookstore.ui;
-
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.server.PWA;
-
-@PWA(name = "Bookstore", shortName = "Bookstore")
-public class AppShell implements AppShellConfigurator {
-}
